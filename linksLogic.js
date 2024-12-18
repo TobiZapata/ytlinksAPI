b79d4c212376b9037951fa3c5b49d8066448462b
Tobi <tobiaszapata99@gmail.com>
2024-12-18 16:50:30 -0300

    asd

--- linksLogic.js
+++ linksLogic.js
@@ -25,8 +25,8 @@ const linksLogic = async (channelLink) => {
       headless: chromium.headless,
       defaultViewport: chromium.defaultViewport,
     });
-
     const page = await browser.newPage();
+    console.log("browser abierto en una nueva ventana");
     await page.setUserAgent(
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
     );
