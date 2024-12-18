1e6de6f1c23940b4794d031154b18c3d31955b16
Tobi <tobiaszapata99@gmail.com>
2024-12-18 19:02:21 -0300

    asds

--- linksLogic.js
+++ linksLogic.js
@@ -7,27 +7,18 @@ const linksLogic = async (channelLink) => {
   try {
     const URL = `${channelLink}/videos`;
     console.log(URL);
-    const isLocal = !!process.env.CHROME_EXECUTABLE_PATH;
+
     const browser = await puppeteer.launch({
-      args: isLocal
-        ? puppeteer.defaultArgs()
-        : [
-            ...chromium.args,
-            "--hide-scrollbars",
-            "--incognito",
-            "--no-sandbox",
-          ],
-      executablePath:
-        process.env.CHROME_EXECUTABLE_PATH ||
-        (await chromium.executablePath(
-          "https://tobiassets.s3.us-east-2.amazonaws.com/chromium-v131.0.1-pack.tar"
-        )),
+      args: [...chromium.args, "--hide-scrollbars", "--incognito"],
+      executablePath: await chromium.executablePath(
+        "https://tobiassets.s3.us-east-2.amazonaws.com/chromium-v131.0.1-pack.tar"
+      ),
       headless: chromium.headless,
       defaultViewport: chromium.defaultViewport,
     });
     const page = await browser.newPage();
     console.log("browser abierto en una nueva ventana");
-    await page.goto(URL);
+    await page.goto(URL, { waitUntil: "domcontentloaded" });
 
     //scroll para cargar todos los video
 
@@ -39,7 +30,7 @@ const linksLogic = async (channelLink) => {
       await page.evaluate(() =>
         window.scrollTo(0, document.documentElement.scrollHeight)
       );
-      await new Promise((resolve) => setTimeout(resolve, 400));
+      await new Promise((resolve) => setTimeout(resolve, 300));
 
       const newHeight = await page.evaluate(
         () => document.documentElement.scrollHeight
